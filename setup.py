f771324eac6c1a0a7c55e47e81bf18f232f1c163
Eric Neuman <soloptimus@gmail.com>
2014-01-23 19:39:45 -0500

    added setup.py so i can use this in pip

---
+++ setup.py
@@ -0,0 +1,20 @@
+#-*- encoding: utf-8 -*-
+setup(name="python-carteblanche",
+      version="0.0.1",
+      description="Module to hold and serialize the in-memory relationship between urls and objects and users.",
+      url="https://github.com/neuman/python-carteblanche",
+      maintainer='Eric Neuman',
+      maintainer_email='eric@indiepen.net',
+      packages=["carteblanche",
+                "carteblanche.models",
+                "carteblanche.tests"],
+      install_requires=[],
+      platforms=['Any'],
+      keywords=['django', 'py-money', 'money'],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Framework :: Django", ]
+      )
