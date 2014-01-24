c88b0685f00a5680a5b79642910b7b32fe014bc4
Eric Neuman <soloptimus@gmail.com>
2014-01-24 02:50:24 -0500

    added caching capabilities

--- carteblanche/models.py
+++ carteblanche/models.py
@@ -6,6 +6,7 @@ class Verb(object):
     '''
     url = "Unspecified"
     display_name = "Unspecified"
+    availability_key = None
 
     def __init__(self, instance=None):
         self.instance = instance
@@ -31,7 +32,6 @@ class Verb(object):
 
 
 class Noun(object):
-    noun_cache = {}
     '''
     Mixin intended to operate with Verb, most likely in a django-like Model or class-based View.
     '''
@@ -39,11 +39,32 @@ class Noun(object):
         return []
 
     def get_available_verbs(self, user):
+        cache.__contains__('is_member')
         output = []
         for a in self.get_verbs():
-            if a.is_available(user):
-                output.append(a.get_serialized())
+            #availability key should be the same for verbs that have
+            #the same is_available method
+            if self.instance.availability_key != None:
+                #if the key exists, the method has been run already so we skip it
+                if cache.__contains__(self.instance.availability_key):
+                    available = cache[self.instance.availability_key]
+                else:
+                    #otherwise we run the method and add it to the cache
+                    available = a.is_available(user):
+                    cache.__setitem__(self.instance.availability_key, available)
+                if available == True:
+                    output.append(a.get_serialized())
         return output
+
+    def try_cache(self, key, value):
+        if self.instance.noun_cache.__contains__('is_member'):
+            print 'using data from cache'
+            return self.instance.noun_cache['is_member']
+        else:
+            print 'add to cache'
+            is_member = self.instance.members.filter(id=user.id).count() > 0
+            self.instance.noun_cache.__setitem__('is_member',is_member)
+        return is_member
 
     class Meta:
         abstract = True
