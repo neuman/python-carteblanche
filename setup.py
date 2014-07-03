0e9c04a40af47e720d5c1f618e8c9423f64c998d
Eric Neuman <soloptimus@gmail.com>
2014-07-03 11:46:49 -0400

    updated setup to reflect version 1.0.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
     name = 'carteblanche',
     packages = ['carteblanche'],
-    version = '0.0.5',
+    version = '1.0.0',
     description='Module to hold and serialize the in-memory relationship between urls and objects and users.',
     maintainer='Eric Neuman',
-    maintainer_email='eric@indiepen.net',
+    maintainer_email='eric@soloptimus.net',
     url="https://github.com/neuman/python-carteblanche",
-    download_url = 'https://github.com/neuman/python-carteblanche/tarball/v0.0.3',
-    keywords=['django', 'navigation', 'menu', 'verb', 'noun']
+    download_url = 'https://github.com/neuman/python-carteblanche/tarball/v1.0.0',
+    keywords=['django', 'navigation', 'menu', 'verb', 'noun','permissions']
 )
